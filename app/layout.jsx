66c89310b215e5efb33721ee9dfad8bfa405458d
Hugo Corona <hugo.corona.r@gmail.com>
2023-08-01 15:46:03 -0700

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: 'Pokedex',
+    description: 'National pokedex powered by PokeAPI'
+
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+        <body>
+            <div className='main'>
+                <div className='gradient'/>
+            </div>
+            <main className='app'>
+                {children}
+            </main>
+        </body>
+
+    </html>
+  )
+}
+
+export default RootLayout
