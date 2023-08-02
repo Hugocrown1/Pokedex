ba7b0cfa55f847874c6a344d7afbd36e62dbc186
Hugo Corona <hugo.corona.r@gmail.com>
2023-08-01 22:17:06 -0700

    favicon added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,16 @@
 import '@styles/globals.css';
 
-export const metadata = {
-    title: 'Pokedex',
-    description: 'National pokedex powered by PokeAPI'
 
+export const metadata = {
+    title: 'Pokedex by bijat',
+    description: 'National pokedex powered by PokeAPI',
+    icon: '/icon.png'
 }
 
 const RootLayout = ({children}) => {
   return (
     <html lang="en">
+        
         <body className='bg-[#10002b]'>
             <div className='main'>
                 <div className='gradient'/>
