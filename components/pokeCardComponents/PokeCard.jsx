f659f21ed7ad9168ede2134d0f840a6b0656532d
Hugo Corona <hugo.corona.r@gmail.com>
2023-08-17 21:48:55 -0700

    tables added

--- components/pokeCardComponents/PokeCard.jsx
+++ components/pokeCardComponents/PokeCard.jsx
@@ -19,12 +19,14 @@ import { pokemonTypes } from '@utils/pokeTypes';
 import { cardStyleMapping } from '@utils/pokeTypes';
 import {CircularProgress} from "@nextui-org/progress";
 
+import { IconGenderFemale, IconGenderMale } from '@tabler/icons-react';
+
 import {Progress} from "@nextui-org/progress";
 import {Chip} from "@nextui-org/chip";
 import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/card";
 import {Divider} from "@nextui-org/divider";
-// import {Image} from "@nextui-org/image";
 import { DefensesTable } from './DefensesTable';
+import { InfoCard } from './InfoCard';
 
 const PokeCard = ({pokemon, specie, weaknesses}) => {
   
@@ -45,77 +47,13 @@ const PokeCard = ({pokemon, specie, weaknesses}) => {
         <h1 className="italic text-center text-5xl font-inter font-semibold mb-5">{pokeNumber}</h1>
 
         <div className='flex  justify-start gap-x-8 items-center  h-full'>
+          
           {/* Carta de pokemon */}
-
-          <div className='relative items-start w-fit mr-20'>
-            <Image
-            isBlurred={true}
-              className='absolute z-10'
-              alt='official Pokémon artwork'
-              src={pokemon.sprites.other['official-artwork'].front_default}
-              width="380"
-              height="380"
-              quality={100}
-              style={{ transform: 'translate(-6%, -10%)', maxWidth: 'none' }}
-            />
-
-            {/* cambia estilo */}
-            <Card className={cardStyle.border}>
-            
-              <CardBody className="overflow-visible justify-end items-center py-8">
-
-              
-                <h3 className='font-semibold text-3xl tracking-wider text-center' >{pokemon.species.name.toUpperCase()}</h3>
-                
-                {/* cambia estilo */}
-                <Divider className={cardStyle.divider}/>
-
-                <h3 className='font-semibold'>{specie.genera[5].genus}</h3>
-              
-              
-              </CardBody>
-              <CardFooter className='justify-center'>
-                <div className='flex flex-row'>
-
-                  <div className='flex flex-col items-center'>
-    
-                    <h3 className='font-semibold'>{(pokemon.height * 0.1).toFixed(1)} m</h3>
-
-                      {/* cambia estilo */}
-                    <Chip className={cardStyle.chip} variant="shadow"><span className='font-semibold'>Altura</span></Chip>
-                  </div>
-
-                  <Divider orientation="vertical" className='mx-2  h-auto bg-gray-600'/>
-
-  
-                  <div className='flex flex-col items-center'>
-    
-                    <div className='flex flex-row mb-1'>{pokemon.types.map((type, index) => <TypeIcon key={index} type={type.type.name}/>)}</div>
-
-                      {/* cambia estilo */}
-                    <Chip className={cardStyle.chip} variant="shadow"><span className='font-semibold'>Tipo</span></Chip>
-                  </div>
-
-                  <Divider orientation="vertical" className='mx-2  h-auto bg-gray-600'/>
-    
-                  <div className='flex flex-col items-center'>
-    
-                    <h3 className='font-semibold'>{(pokemon.weight * 0.1).toFixed(1)} kg</h3>
-
-                    {/* cambia estilo */}
-                    <Chip className={cardStyle.chip} variant="shadow"><span className='font-semibold'>Peso</span></Chip>
-                  </div>
-
-                </div>
-
-
-              </CardFooter>
-            </Card>
-
-          </div>
+          <InfoCard pokemon={pokemon} specie={specie}/>
 
           <div className='flex flex-col'>
             <div className='flex flex-row gap-x-4 items-center'>
+              
               <div className='flex flex-col font-semibold'>
               <h2 className=' text-left text-2xl font-inter pl-2 font-bold mb-2'>Datos</h2>
                   <Table isSticky isStriped className='text-left w-full font-semibold' hideHeader aria-label="Misc table">
@@ -242,14 +180,35 @@ const PokeCard = ({pokemon, specie, weaknesses}) => {
                       <TableCell>Grupos huevo: </TableCell>
                       <TableCell className='text-left'>
                         {specie.egg_groups.map((group, index) => (
-                          index === (specie.egg_groups.length - 1) ?  <span key={index}>{eggGroupTranslations[group.name]}</span> : <span key={index}>{eggGroupTranslations[group.name]}, </span>
+                          index === (specie.egg_groups.length - 1) ?  
+                          <span key={index}>{eggGroupTranslations[group.name]}</span> 
+                          : 
+                          <span key={index}>{eggGroupTranslations[group.name]}, </span>
                         ))}
                       </TableCell>
                     </TableRow>
                     <TableRow key="2">
                       <TableCell >Genero: </TableCell>
-                      <TableCell >
-                      
+                      <TableCell className='flex flex-row'>
+                         { specie.gender_rate < 0 ? (<span>Sin genero</span>) :
+                          (<>
+                          <IconGenderMale
+                          color='#2137fd'
+                          /> {100 / 8 * (8 - specie.gender_rate ) }%
+                          <Divider orientation="vertical" className='mx-2  h-auto bg-gray-600'/>
+                          <IconGenderFemale
+                          color='#c514e9'
+                          /> { 100 / 8 * specie.gender_rate }%
+                          </>
+                          ) }
+
+                      </TableCell>
+                    </TableRow>
+
+                    <TableRow key="3">
+                      <TableCell >Pasos: </TableCell>
+                      <TableCell className='flex flex-row'>
+                          {255 * (specie.hatch_counter + 1)}
                       </TableCell>
                     </TableRow>
                   </TableBody>
